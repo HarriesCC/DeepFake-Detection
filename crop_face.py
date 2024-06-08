74fb93e0be2f9bee18d7600d643e9f32da08a8fc
HarriesChen <harrieschen@gmail.com>
2024-06-07 23:28:32 -0700

    Add video to image and face crop

---
+++ crop_face.py
@@ -0,0 +1,67 @@
+import cv2
+from mtcnn import MTCNN
+import os
+import json
+import tensorflow as tf
+from keras import backend as K
+
+def configure_tensorflow():
+    print(tf.__version__)
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+    physical_devices = tf.config.list_physical_devices('GPU')
+    print(physical_devices)
+    if physical_devices:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+def get_filename_only(file_path):
+    file_basename = os.path.basename(file_path)
+    filename_only = file_basename.split('.')[0]
+    return filename_only
+
+def process_videos(base_path):
+    with open(os.path.join(base_path, 'metadata.json')) as metadata_json:
+        metadata = json.load(metadata_json)
+        print(f"Total files in metadata: {len(metadata)}")
+
+    for filename in metadata.keys():
+        tmp_path = os.path.join(base_path, get_filename_only(filename))
+        print(f'Processing Directory: {tmp_path}')
+        frame_images = [x for x in os.listdir(tmp_path) if os.path.isfile(os.path.join(tmp_path, x))]
+        faces_path = os.path.join(tmp_path, 'faces')
+        print(f'Creating Directory: {faces_path}')
+        os.makedirs(faces_path, exist_ok=True)
+        print('Cropping Faces from Images...')
+
+        for frame in frame_images:
+            print(f'Processing {frame}')
+            detector = MTCNN()
+            image = cv2.cvtColor(cv2.imread(os.path.join(tmp_path, frame)), cv2.COLOR_BGR2RGB)
+            results = detector.detect_faces(image)
+            print(f'Face Detected: {len(results)}')
+            count = 0
+
+            for result in results:
+                bounding_box = result['box']
+                print(bounding_box)
+                confidence = result['confidence']
+                print(confidence)
+                if len(results) < 2 or confidence > 0.95:
+                    margin_x = bounding_box[2] * 0.3  # 30% as the margin
+                    margin_y = bounding_box[3] * 0.3  # 30% as the margin
+                    x1 = max(int(bounding_box[0] - margin_x), 0)
+                    x2 = min(int(bounding_box[0] + bounding_box[2] + margin_x), image.shape[1])
+                    y1 = max(int(bounding_box[1] - margin_y), 0)
+                    y2 = min(int(bounding_box[1] + bounding_box[3] + margin_y), image.shape[0])
+                    print(x1, y1, x2, y2)
+                    crop_image = image[y1:y2, x1:x2]
+                    new_filename = '{}-{:02d}.png'.format(os.path.join(faces_path, get_filename_only(frame)), count)
+                    count += 1
+                    cv2.imwrite(new_filename, cv2.cvtColor(crop_image, cv2.COLOR_RGB2BGR))
+                else:
+                    print('Skipped a face..')
+
+# Example usage
+if __name__ == "__main__":
+    base_path = './train_sample_videos/'
+    configure_tensorflow()
+    process_videos(base_path)
---
+++ video_to_image.py
@@ -0,0 +1,57 @@
+import json
+import os
+import cv2
+import math
+
+
+def process_videos(base_path):
+    def get_filename_only(file_path):
+        file_basename = os.path.basename(file_path)
+        filename_only = file_basename.split('.')[0]
+        return filename_only
+
+    with open(os.path.join(base_path, 'metadata.json')) as metadata_json:
+        metadata = json.load(metadata_json)
+        print(f"Total files in metadata: {len(metadata)}")
+
+    for filename in metadata.keys():
+        print(f"Processing file: {filename}")
+        if filename.endswith(".mp4"):
+            tmp_path = os.path.join(base_path, get_filename_only(filename))
+            print(f'Creating Directory: {tmp_path}')
+            os.makedirs(tmp_path, exist_ok=True)
+            print('Converting Video to Images...')
+            count = 0
+            video_file = os.path.join(base_path, filename)
+            cap = cv2.VideoCapture(video_file)
+            frame_rate = cap.get(cv2.CAP_PROP_FPS)
+            while cap.isOpened():
+                frame_id = cap.get(cv2.CAP_PROP_POS_FRAMES)
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                if frame_id % math.floor(frame_rate) == 0:
+                    print('Original Dimensions: ', frame.shape)
+                    if frame.shape[1] < 300:
+                        scale_ratio = 2
+                    elif frame.shape[1] > 1900:
+                        scale_ratio = 0.33
+                    elif 1000 < frame.shape[1] <= 1900:
+                        scale_ratio = 0.5
+                    else:
+                        scale_ratio = 1
+                    print('Scale Ratio: ', scale_ratio)
+
+                    width = int(frame.shape[1] * scale_ratio)
+                    height = int(frame.shape[0] * scale_ratio)
+                    dim = (width, height)
+                    new_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+                    print('Resized Dimensions: ', new_frame.shape)
+
+                    new_filename = '{}-{:03d}.png'.format(os.path.join(tmp_path, get_filename_only(filename)), count)
+                    count += 1
+                    cv2.imwrite(new_filename, new_frame)
+            cap.release()
+            print("Done!")
+        else:
+            continue
